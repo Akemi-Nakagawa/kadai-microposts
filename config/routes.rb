d50d9ee0127889551062a8ed6fea2d9dd3c7f977
nkgwakm <nkgwakemi@gmail.com>
2020-04-13 10:33:50 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'microposts/create'
-  get 'microposts/destroy'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
@@ -16,7 +6,15 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to:'users#new' 
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create] do
+      member do
+        get :followings
+        get :followers
+      end
+  end
+
   resources :users, only: [:index, :show, :new, :create]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
